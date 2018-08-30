a94dd5b7221ffec98e05b95583661fe82efef6f2
Captain <mads130f@edu.easj.dk>
2018-08-30 17:29:22 +0200

    testing fra termnial

---
+++ yeah.js
@@ -0,0 +1,26 @@
+var counter = 1;
+function myFunction(){
+    var a = new Audio("https://www.myinstants.com/media/sounds/yasuo.mp3");
+    var b = new Audio("https://www.myinstants.com/media/sounds/7458_yasuo.mp3");
+    if (counter%4==0){
+       a.play();
+       addNado();
+        counter++;
+    }else{
+        b.play();
+       
+    }
+   
+
+   counter++;
+   console.log(counter);
+}
+function addNado(){
+    console.log("it runs");
+    src = "http://rs387.pbsrc.com/albums/oo312/KirkWasHere/tornado.gif~c200";
+    x  = document.createElement('img');
+    x.src = src;
+    document.body.appendChild(x);
+    
+ 
+}
